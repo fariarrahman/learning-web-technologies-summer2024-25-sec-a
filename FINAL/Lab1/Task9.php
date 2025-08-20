0665bc2893d1777f773a69455e8ae84a1e17bd5a
Fariar Rahman <fariarrahman01@gmail.com>
2025-08-20 16:23:59 +0600

    Add files via upload

---
+++ FINAL/Lab1/Task9.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>Login Form</title>
+</head>
+<body>
+    <h1>PHP Log in Form</h1>
+    <hr><br>
+
+    <form method="post">       
+        <label for="email">Email Address:</label>
+        <input type="email" id="email" name="email" placeholder="abc@email.com"><br><br>
+
+        <label for="password">Password:</label>
+        <input type="password" id="password" placeholder="Password" name="password"><br><br>
+
+        <input type="reset">
+        <input type="submit" value="Log in">
+        <br><br>
+
+        <p>Don't have an account? <a href="registration_form.html">Sign up!!!</a></p>
+    </form>
+
+    <?php 
+        // Corrected function name and superglobal variable case
+        print_r($_POST);
+    ?>
+</body>
+</html>
